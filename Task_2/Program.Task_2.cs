c7230e464b9d82c44cec961a6056ed6e2a99b42d
Dima Ivanov <ivanovdimat5@gmail.com>
2023-01-10 20:18:25 +0300

    Решили вторую задачу

---
+++ Task_2/Program.Task_2.cs
@@ -0,0 +1,13 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число: ");
+string N = Console.ReadLine();
+
+try
+{
+    Console.WriteLine($"Третьей цифрой числа является: {N[2]}.");
+}
+catch (System.Exception)
+{
+   Console.WriteLine("Число не имеет третьей цифры!");
+
+}
